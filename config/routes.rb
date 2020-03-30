7b08499e3dccc11254f2d9a91e897334800fb74a
Warren Harper II <warrenharper2@att.net>
2020-03-30 20:09:18 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index, :create]
-  get '/students/new', to: 'students#new', as: 'new_student'
+  #resources :students, only: [:index, :create]
+
+
+  #get '/students/new', to: 'students#new', as: 'new_student'
   get '/students/:id', to: 'students#show', as: 'student'
+
+  resources :students, only: [:index, :new, :create]
+
+
 end
