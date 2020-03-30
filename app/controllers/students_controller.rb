be65830a3694c9d29a460478d31c8aff7f69915b
Warren Harper II <warrenharper2@att.net>
2020-03-30 20:15:32 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -10,7 +10,7 @@ class StudentsController < ApplicationController
 
   def new
   end
-  
+
   def create
     Student.create(first_name: params[:first_name], last_name: params[:last_name])
     redirect_to students_path
